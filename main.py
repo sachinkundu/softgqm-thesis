ac2cb11c8fc7f7671c94588a1fbaa813c4e7d00a
Sachin Kundu <sachin.kundu@pm.me>
2024-01-26 17:17:33 +0200

    trajectory control with position feedback, but orientation is still open loop

--- main.py
+++ main.py
@@ -19,7 +19,7 @@ Tf = 0.1 * (N - 1)
 
 no_of_simulations = 1
 
-include_cloth = True
+include_cloth = False
 
 
 def grasp_imp(env, state):
@@ -89,6 +89,14 @@ def main():
             logging.info(f"cloth angle: {np.rad2deg(tr.quat_angle(tr.hmat_to_pos_quat(cloth_in_eef_SE3)[1]))}")
             trajectory_space_frame = mr.CartesianTrajectory(eef_SE3, cloth_SE3, Tf, N, 5)
             trajectory_eef_frame = mr.CartesianTrajectory(eef_SE3, cloth_in_eef_SE3, Tf, N, 5)
+
+            cube_SE3 = tr.pos_quat_to_hmat(initial_state['cube_pos'], initial_state['cube_quat'])
+            cube_in_eef_SE3 = np.matmul(mr.TransInv(eef_SE3), cube_SE3)
+            logging.info(f"cube angle: {np.rad2deg(tr.quat_angle(tr.hmat_to_pos_quat(cube_in_eef_SE3)[1]))}")
+
+            trajectory_space_frame = mr.CartesianTrajectory(eef_SE3, cube_SE3, Tf, N, 5)
+            trajectory_eef_frame = mr.CartesianTrajectory(eef_SE3, cube_in_eef_SE3, Tf, N, 5)
+
         else:
             cube_SE3 = tr.pos_quat_to_hmat(initial_state['cube_pos'], initial_state['cube_quat'])
             cube_in_eef_SE3 = np.matmul(mr.TransInv(eef_SE3), cube_SE3)
@@ -97,53 +105,57 @@ def main():
             trajectory_space_frame = mr.CartesianTrajectory(eef_SE3, cube_SE3, Tf, N, 5)
             trajectory_eef_frame = mr.CartesianTrajectory(eef_SE3, cube_in_eef_SE3, Tf, N, 5)
 
-        last_obs = initial_state
+        p_gain = 20
+        ang_gain = 2.8
 
-        p_gain = 25
-        ang_gain = 2
+        current_eef_pos = trajectory_space_frame[0][:-1, -1]
+        last_obs = initial_state
+        for i, (destination_pose, frame1_e, frame2_e) in enumerate(zip(trajectory_space_frame,
+                                                                       trajectory_eef_frame,
+                                                                       trajectory_eef_frame[1:])):
+
+            if np.linalg.norm(current_eef_pos - initial_state['cube_pos']) < 0.01:
+                break
+
+            frame2_pos = destination_pose[:-1, -1]
+            logging.info(f"taking step: {i}")
+            while not np.allclose(frame2_pos, current_eef_pos, rtol=0.005, atol=0.005):
+                logging.info(f"taking step: {i}")
+                pos_diff = p_gain * (frame2_pos - current_eef_pos)
+
+                frame1_e_ax_ang = tr.quat_to_axisangle(tr.hmat_to_pos_quat(frame1_e)[1])
+                frame2_e_ax_ang = tr.quat_to_axisangle(tr.hmat_to_pos_quat(frame2_e)[1])
+
+                ang_diff = ang_gain * (frame2_e_ax_ang - frame1_e_ax_ang)
+
+                # ang_diff = np.zeros(shape=(3, ))
+
+                action = np.array([pos_diff[0],
+                                   pos_diff[1],
+                                   pos_diff[2],
+                                   ang_diff[0],
+                                   ang_diff[1],
+                                   ang_diff[2],
+                                   -1])
+                obs, reward, done, _ = env.step(action.tolist())
+                current_eef_pos = obs['robot0_eef_pos']
+                env.render()
+
+                logging.debug(
+                    f"eef_axis: {tr.quat_axis(obs['robot0_eef_quat'])} : eef_angle: {np.rad2deg(tr.quat_angle(obs['robot0_eef_quat']))}")
+                logging.debug(f"cube_pos: {obs['cube_pos']} eef_pos: {obs['robot0_eef_pos']}")
+                last_obs = obs
 
         if include_cloth:
-            p_gain = 50
-            ang_gain = 2.8
+            logging.info(f"cloth pos error: {np.linalg.norm(last_obs['cloth_pos'] - last_obs['robot0_eef_pos'])}")
+            logging.info(
+                f"cloth ang error: {np.rad2deg(tr.quat_angle(tr.hmat_to_pos_quat(cloth_in_eef_SE3)[1]) - tr.quat_angle(last_obs['cloth_quat']))}")
+        else:
+            logging.info(f"cube pos error: {np.linalg.norm(last_obs['cube_pos'] - last_obs['robot0_eef_pos'])}")
+            logging.info(
+                f"cube ang error: {np.rad2deg(tr.quat_angle(tr.hmat_to_pos_quat(cube_in_eef_SE3)[1]) - tr.quat_angle(last_obs['robot0_eef_quat']))}")
 
-        for i, (frame1_s, frame2_s, frame1_e, frame2_e) in enumerate(
-                zip(trajectory_space_frame, trajectory_space_frame[1:],
-                    trajectory_eef_frame, trajectory_eef_frame[1:])):
-            frame1_pos = frame1_s[:-1, -1]
-            frame2_pos = frame2_s[:-1, -1]
-
-            pos_diff = p_gain * (frame2_pos - frame1_pos)
-
-            frame1_e_ax_ang = tr.quat_to_axisangle(tr.hmat_to_pos_quat(frame1_e)[1])
-            frame2_e_ax_ang = tr.quat_to_axisangle(tr.hmat_to_pos_quat(frame2_e)[1])
-
-            ang_diff = ang_gain * (frame2_e_ax_ang - frame1_e_ax_ang)
-
-            action = np.array([pos_diff[0],
-                               pos_diff[1],
-                               pos_diff[2],
-                               ang_diff[0],
-                               ang_diff[1],
-                               ang_diff[2],
-                               -1])
-            obs, reward, done, _ = env.step(action.tolist())
-            env.render()
-
-            logging.debug(
-                f"eef_axis: {tr.quat_axis(obs['robot0_eef_quat'])} : eef_angle: {np.rad2deg(tr.quat_angle(obs['robot0_eef_quat']))}")
-            logging.debug(f"cube_pos: {obs['cube_pos']} eef_pos: {obs['robot0_eef_pos']}")
-            last_obs = obs
-            if i == len(trajectory_space_frame) - 2:
-                if include_cloth:
-                    logging.info(f"cloth pos error: {np.linalg.norm(obs['cloth_pos'] - obs['robot0_eef_pos'])}")
-                    logging.info(
-                        f"cloth ang error: {np.rad2deg(tr.quat_angle(tr.hmat_to_pos_quat(cloth_in_eef_SE3)[1]) - tr.quat_angle(obs['cloth_quat']))}")
-                else:
-                    logging.info(f"cube pos error: {np.linalg.norm(obs['cube_pos'] - obs['robot0_eef_pos'])}")
-                    logging.info(
-                        f"cube ang error: {np.rad2deg(tr.quat_angle(tr.hmat_to_pos_quat(cube_in_eef_SE3)[1]) - tr.quat_angle(obs['robot0_eef_quat']))}")
-
-        cv2.waitKey(3000)
+        cv2.waitKey(1000)
     env.close()
 
 
